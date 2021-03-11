50b222ec2c7dd8a19aba57c16d1f19686d02652c
Ethan <etha.m.sylvester@gmail.com>
2021-03-11 17:55:52 +1000

    Refactor broken scores for functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -3,12 +3,23 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("invalid input")
-elif score > 90:
-    print("score is excellent")
-elif score > 50:
-    print("score is passable")
-else:
-    print("score is bad")
+
+def main():
+    score = float(input("Enter score: "))
+    result = determine_status(score)
+    print(result)
+
+
+def determine_status(score):
+    if score < 0 or score > 100:
+        result = "invalid input"
+    elif score > 90:
+        result = "score is excellent"
+    elif score > 50:
+        result = "score is passable"
+    else:
+        result = "score is bad"
+    return result
+
+
+main()
