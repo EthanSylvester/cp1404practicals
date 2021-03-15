3417a0ada7f07bbfda0380638cace6a995175e36
Ethan <etha.m.sylvester@gmail.com>
2021-03-16 09:52:28 +1000

    Add lists warmup and total income programs

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,18 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+# Guesses for answers
+# Null | Null | 4 | Null | 4, 1 | 1 | 0 | 0 | 3, 1, 4, 1, 5, 9, 2, 6, 5, 3 |
+print(numbers[0])
+print(numbers[-1])
+print(numbers[3])
+print(numbers[:-1])
+print(numbers[3:4])
+print(5 in numbers)
+print(7 in numbers)
+print("3" in numbers)
+print(numbers + [6, 5, 3])
+# Actual results
+# 3 | 2 | 1 | 3, 1, 4, 1, 5, 9 | 1 | True | False | False | 3, 1, 4, 1, 5, 9, 2, 6, 5, 3 |
+numbers[0] = "ten"
+numbers[-1] = 1
+print(numbers[2:])
+print(9 in numbers)
