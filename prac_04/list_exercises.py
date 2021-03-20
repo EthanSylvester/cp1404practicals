414183e3ccc9e205b6780df0acb5d0f9c3913904
Ethan <etha.m.sylvester@gmail.com>
2021-03-20 11:58:40 +1000

    Add username checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -9,6 +9,17 @@ def main():
     print("The smallest number is {}".format(min(numbers)))
     print("The largest number is {}".format(max(numbers)))
     print("The average of the numbers is {}".format(sum(numbers)/len(numbers)))
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+    user = input("Please input username: ")
+    checker = False
+    for i in range(0, len(usernames)):
+        if user == usernames[i]:
+            print("Access Granted")
+            checker = True
+    if checker is False:
+        print("Access Denied")
 
 
 main()
