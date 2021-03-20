21c1edd701b81dae41b06715ab7c0799ab020da0
Ethan <etha.m.sylvester@gmail.com>
2021-03-20 11:15:39 +1000

    Return a list of lists of subject data from get_data

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -3,7 +3,7 @@ CP1404/CP5632 Practical
 Data file -> lists program
 """
 
-FILENAME = "subject_data.txt.txt"
+FILENAME = "subject_data.txt"
 
 
 def main():
@@ -14,6 +14,7 @@ def main():
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    count_lines = 0
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -23,7 +24,19 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        count_lines += 1
     input_file.close()
+    input_file = open(FILENAME)
+    list_lines = [i for i in range(count_lines)]
+    index = 0
+    for line in input_file:
+        line = line.strip()
+        parts = line.split(',')
+        parts[2] = int(parts[2])
+        list_lines[index] = parts
+        index += 1
+    input_file.close()
+    return list_lines
 
 
 main()
