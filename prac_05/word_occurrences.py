2ad7f81fdb81368d7769f127184fadc0c77a1e2d
Ethan <etha.m.sylvester@gmail.com>
2021-03-25 11:39:36 +1000

    Add word occurrences program

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,10 @@
+user_text = input("Text: ")
+user_text = user_text.split()
+longest = 1
+for i in user_text:
+    if len(i) > longest:
+        longest = len(i)
+longest += 1
+word_counts = {k: user_text.count(k) for k in user_text}
+for key, value in word_counts.items():
+    print("{} : {:>{}}".format(key, value, longest - len(key)))
