165865434877697cad0fc9b71ac57c1161c0c343
Ethan <etha.m.sylvester@gmail.com>
2021-03-25 10:24:55 +1000

    Print all states at end of state names

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -16,3 +16,6 @@ while state_code != "":
     else:
         print("Invalid short state")
     state_code = input("Enter short state: ")
+print("The Australian states are:")
+for key, value in CODE_TO_NAME.items():
+    print("{:<3} is {}".format(key, value))
