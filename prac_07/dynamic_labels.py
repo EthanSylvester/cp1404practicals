5d6cd52759b9173aefb236382799f968651d8551
ethan <ethan.sylvester@my.jcu.edu.au>
2021-04-23 15:10:50 +1000

    Refactor dynamic labels so that the creation of labels is it's own function

--- prac_07/dynamic_labels.py
+++ prac_07/dynamic_labels.py
@@ -1,6 +1,6 @@
 from kivy.app import App
 from kivy.lang import Builder
-from  kivy.uix.label import Label
+from kivy.uix.label import Label
 
 
 class DynamicLabels(App):
@@ -11,10 +11,13 @@ class DynamicLabels(App):
     def build(self):
         self.title = "Dynamic Labels"
         self.root = Builder.load_file("dynamic_labels.kv")
+        self.create_labels()
+        return self.root
+
+    def create_labels(self):
         for name in self.names:
             temp_label = Label(text=name)
             self.root.ids.entries_box.add_widget(temp_label)
-        return self.root
 
 
 DynamicLabels().run()
