471b50a26bc8c00786f229af12a707556287a034
ethan <ethan.sylvester@my.jcu.edu.au>
2021-04-23 14:19:42 +1000

    Edit dynamic labels python program to generate labels

--- prac_07/dynamic_labels.py
+++ prac_07/dynamic_labels.py
@@ -1,11 +1,19 @@
 from kivy.app import App
 from kivy.lang import Builder
+from  kivy.uix.label import Label
 
 
 class DynamicLabels(App):
+    def __init__(self):
+        super().__init__()
+        self.names = ["Ethan", "David", "Joel"]
+
     def build(self):
         self.title = "Dynamic Labels"
         self.root = Builder.load_file("dynamic_labels.kv")
+        for name in self.names:
+            temp_label = Label(text=name)
+            self.root.ids.entries_box.add_widget(temp_label)
         return self.root
 
 
